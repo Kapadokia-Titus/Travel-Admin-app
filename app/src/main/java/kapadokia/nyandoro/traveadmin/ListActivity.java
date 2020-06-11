10508e131799b4c710842966ed5b2a39748d4fa7
Kapadokia-Titus <ktnyandoch@gmail.com>
2020-06-11 03:55:05 +0300

    firebase push fxn success

---
+++ app/src/main/java/kapadokia/nyandoro/traveadmin/ListActivity.java
@@ -0,0 +1,68 @@
+package kapadokia.nyandoro.traveadmin;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+public class ListActivity extends AppCompatActivity {
+
+    private ArrayList<TravelDeal> deals;
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
+    private ChildEventListener childEventListener;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+        //inits
+        deals = new ArrayList<>();
+
+        textView = findViewById(R.id.tv_deals);
+
+
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference().child("traveldeals");
+        childEventListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                TravelDeal td = dataSnapshot.getValue(TravelDeal.class);
+                textView.setText(td.getTitle()+" \n"+td.getDescription()+"\n"+td.getPrice());
+                databaseReference.addChildEventListener(childEventListener);
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        };
+    }
+}
